5fa1b4c99a25c49e9009d62998ee327487af255c
Helen Lauren <helenbonato02@gmail.com>
2025-04-03 13:21:24 -0300

    primeira aula

--- PrimeiroProjetoC#/Program.cs
+++ PrimeiroProjetoC#/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Screen Sound
 
+//primeira aula
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+Console.WriteLine(mensagemDeBoasVindas);
+
+//exercicio prático 1
+string curso = "C# primeira aplicação";
+Console.WriteLine(curso);
+
+
